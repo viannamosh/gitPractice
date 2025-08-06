7d64705d5911fff448d653e652e3a911a1124e6b
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-06 11:30:13 -0700

    Initial Commit

---
+++ gitPractice/gitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeApp.swift
+//  gitPractice
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct gitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
