c9774dc955be2bb91c6a9f5795f67ec8aed68961
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-06 11:35:11 -0700

    added in change1

--- gitPractice/ContentView.swift
+++ gitPractice/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Text("Change 1")
         }
         .padding()
     }
